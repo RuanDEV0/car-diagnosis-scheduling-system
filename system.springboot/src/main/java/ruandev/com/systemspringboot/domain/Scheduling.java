79711f108ec3494b305a39bc933bb16e6806dac8
Ruan <oliveiraruan2018@gmail.com>
2024-11-19 12:22:58 -0400

    adjusted in relationship in scheduling entity

--- system.springboot/src/main/java/ruandev/com/systemspringboot/domain/Scheduling.java
+++ system.springboot/src/main/java/ruandev/com/systemspringboot/domain/Scheduling.java
@@ -21,10 +21,17 @@ public class Scheduling {
     private Long id;
     private LocalDate data;
     @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "service_scheduling",
+            joinColumns = @JoinColumn(name = "id_scheduling"),
+            inverseJoinColumns = @JoinColumn(name = "id_service")
+
+    )
     private List<Servico> serviceList;
     private String status;
     @ManyToOne(cascade = CascadeType.ALL)
     private Vehicle vehicle;
     @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_client")
     private Client client;
 }
